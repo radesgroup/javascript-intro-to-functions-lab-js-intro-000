d8aa672e5df4dcad40e98593d25f4577f4fd4c2f
Aleksey <fduchunru@gmail.com>
2020-09-26 04:23:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === lowercase)
+  {return "I can't hear you!"}
+  else if (string.toUpperCase() === uppercase)
+  {return "YES INDEED!"}
+  else if (string === "I love you, Grandma")
+  {return "I love you, too"}
+}
